7cf18fdbbff75242e07b508b7759a2cb4f59f632
lausiklauskn-png <lausiklaus.kn@googlemail.com>
2026-03-11 05:47:53 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'rezeptbuch-v1';
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE)
-    .then(c => c.addAll(['/muttis-rezeptbuch/']))));
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request)
-    .then(r => r || fetch(e.request))));
